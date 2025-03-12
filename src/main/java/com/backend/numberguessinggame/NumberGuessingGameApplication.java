d262f7be88337a9d26a6939c4f0ad7506154bbc4
Sukrut K Pasumarthi <kesarisukrut@gmail.com>
2025-03-12 11:08:09 -0400

    initial project setup

---
+++ src/main/java/com/backend/numberguessinggame/NumberGuessingGameApplication.java
@@ -0,0 +1,13 @@
+package com.backend.numberguessinggame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NumberGuessingGameApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NumberGuessingGameApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/backend/numberguessinggame/NumberGuessingGameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.numberguessinggame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NumberGuessingGameApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
