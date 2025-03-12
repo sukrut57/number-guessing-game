c2c682e338c958d9d2d7a4f1b1135180f79d169e
Sukrut K Pasumarthi <kesarisukrut@gmail.com>
2025-03-12 15:23:51 -0400

    added hadling for exceptions and edge cases

--- src/main/java/com/backend/numberguessinggame/commands/GameComponent.java
+++ src/main/java/com/backend/numberguessinggame/commands/GameComponent.java
@@ -2,7 +2,6 @@ package com.backend.numberguessinggame.commands;
 
 import org.springframework.shell.standard.ShellComponent;
 import org.springframework.shell.standard.ShellMethod;
-import org.springframework.shell.standard.ShellOption;
 
 import java.util.Scanner;
 
@@ -13,8 +12,6 @@ public class GameComponent {
 
     int  numberOfAttempts = 0;
     int randomNumber = (int) (Math.random() * 100) + 1;
-    int gameDifficulty;
-
 
     @ShellMethod(key = "start", value = "Start a new game")
     public void startGame() {
@@ -28,28 +25,36 @@ public class GameComponent {
         Scanner scanner = new Scanner(System.in);
         int difficulty = scanner.nextInt();
 
-        switch (difficulty) {
-            case 1:
-                numberOfAttempts = 10;
-                out.print("Great! You have selected the Easy difficulty level.\n" +
-                        "Let's start the game!\n");
-                break;
-            case 2:
-                numberOfAttempts = 5;
-
-                out.print("Great! You have selected the Medium difficulty level.\n" +
-                        "Let's start the game!\n");
-                break;
-            case 3:
-                numberOfAttempts = 3;
-                out.print("Great! You have selected the Hard difficulty level.\n" +
-                        "Let's start the game!\n");
-                break;
-
-            default:
-                out.print("Invalid choice. Please select a valid difficulty level.\n");
-                break;
+        while(difficulty < 1 || difficulty > 3){
+            out.print("Invalid choice. Please select a valid difficulty level.\n");
+            out.print("Enter your choice: ");
+            difficulty = scanner.nextInt();
         }
+
+        switch (difficulty) {
+                case 1:
+                    numberOfAttempts = 10;
+                    out.print("Great! You have selected the Easy difficulty level.\n" +
+                            "Let's start the game!\n");
+                    break;
+
+                case 2:
+                    numberOfAttempts = 5;
+
+                    out.print("Great! You have selected the Medium difficulty level.\n" +
+                            "Let's start the game!\n");
+                    break;
+
+                case 3:
+                    numberOfAttempts = 3;
+                    out.print("Great! You have selected the Hard difficulty level.\n" +
+                            "Let's start the game!\n");
+                    break;
+
+                default:
+                    out.print("Invalid choice. Please select a valid difficulty level.\n");
+                    break;
+            }
     }
 
     private void playGame() {
@@ -57,19 +62,25 @@ public class GameComponent {
 
             Scanner scanner = new Scanner(System.in);
             out.print("Enter your guess: ");
-            int userGuess = scanner.nextInt();
-            if (userGuess == randomNumber) {
-                out.println("Congratulations! You have guessed the correct number in " + numberOfAttempts + " attempts.");
-                break;
-            } else {
-                numberOfAttempts--;
-                out.println("Incorrect! The number is " + (userGuess > randomNumber ? "less" : "greater") + " than " + userGuess);
-                out.println("You have " + numberOfAttempts + " attempts left.");
+            try{
+                int userGuess = scanner.nextInt();
+                if (userGuess == randomNumber) {
+                    out.println("Congratulations! You have guessed the correct number in " + numberOfAttempts + " attempts.");
+                    break;
+                } else {
+                    numberOfAttempts--;
+                    out.println("Incorrect! The number is " + (userGuess > randomNumber ? "less" : "greater") + " than " + userGuess);
+                    out.println("You have " + numberOfAttempts + " attempts left.");
 
+                }
+                if (numberOfAttempts == 0) {
+                    out.println("Game Over! The correct number was " + randomNumber);
+                }
             }
-            if (numberOfAttempts == 0) {
-                out.println("Game Over! The correct number was " + randomNumber);
+            catch (Exception e){
+                out.println("Invalid input. Please enter a valid number.");
             }
+
         }
     }
 
